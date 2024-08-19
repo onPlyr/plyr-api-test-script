130401fc1157c14f46b454de454f5cf5918156d3
CryptoFennec <suwichalala@gmail.com>
2024-08-19 17:12:54 +0700

    add Airdrop API

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { userLogin, userLogout, checkSessionJwt, getUserInfo } = require("./lib/user");
 
-const { airdropCampaignInfo } = require("./lib/airdrop");
+const { airdropCampaignInfo, airdropCampaignClaimableRewards, airdropCampaignClaim } = require("./lib/airdrop");
+
+const { getTaskMessageStatus } = require("./lib/message");
 
 // CLI //
 const args = process.argv.splice(2);
@@ -9,6 +11,7 @@ const args = process.argv.splice(2);
 // You can use 1h , 1d , 1w too.
 // default is 86400s -- in case, you don't specify it
 if (args[0] == 'login') {
+
     userLogin(args[1], args[2], args[3], (args[4] || null));
 
     // node index.js logout eyJhbGciOiJFUzI1N...
@@ -23,6 +26,23 @@ if (args[0] == 'login') {
 else if (args[0] == 'info') {
     getUserInfo(args[1]);
 }
-else if (args[0] == "airdropInfo") {
-    airdropCampaignInfo(args[1]);
+// node index.js airdropInfo
+else if (args[0] == "airdropCampaignInfo") {
+    airdropCampaignInfo();
+}
+// node index.js airdropCheckClaimableRewards 0 0xabc..1234
+else if (args[0] == "airdropCheckClaimableRewards") {
+    args[1] = Number(args[1]);
+    airdropCampaignClaimableRewards(args[1], args[2]);
+}
+
+// node index.js airdropClaim 0 0xabc..1234 true/false
+else if (args[0] == "airdropClaim") {
+    args[1] = Number(args[1]);
+    args[3] = args[3] === "true" ? true : false;
+    airdropCampaignClaim(args[1], args[2], args[3]);
+}
+// node index.js airdropClaimedStatus MESSAGE_ID
+else if (args[0] == 'airdropClaimedStatus') {
+    getTaskMessageStatus(args[1]);
 }
