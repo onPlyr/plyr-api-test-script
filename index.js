0b6d2f77982c1dd9a8116244d8ef46c3a10e4f50
CryptoFennec <suwichalala@gmail.com>
2024-09-04 21:53:33 +0700

    add game api - approve , pay ,earn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { getTaskMessageStatus } = require("./lib/message");
 
 const { getSessionJwtPublicKey, verifyJwtLocally } = require("./lib/jwt");
 
-const { createGameRoom, joinGameRoom, leaveGameRoom } = require("./lib/game");
+const { createGameRoom, joinGameRoom, leaveGameRoom, approveGameToken, payGameRoom, earnGameRoom } = require("./lib/game");
 
 // CLI //
 const args = process.argv.splice(2);
@@ -47,8 +47,7 @@ else if (args[0] == 'getSessionJwtPublicKey') {
 // For a job like, to check if a message is expired or not and then return user back to login page.
 // It's just a simple feature to reduce to checking time.
 // Anyway, you can use /api/user/session/verify if you want accurate result.
-else if (args[0] == 'verifyJwtLocally')
-{
+else if (args[0] == 'verifyJwtLocally') {
     verifyJwtLocally(args[1])
 }
 
@@ -79,8 +78,7 @@ else if (args[0] == 'getTaskMessageStatus') {
 }
 
 // node index.js airdropUserRewards 0 0xabc..1234
-else if (args[0] == 'airdropUserStats')
-{
+else if (args[0] == 'airdropUserStats') {
     airdropUserStats(args[1], args[2]);
 }
 
@@ -100,7 +98,7 @@ else if (args[0] == 'createGameRoom') {
 }
 
 
-// node index.js joinGameRoom 6 sessionJwt
+// node index.js joinGameRoom roomId sessionJwt
 // args1 = roomId
 // args2 = plyrId
 // args3 = sessionJwt
@@ -109,7 +107,7 @@ else if (args[0] == 'joinGameRoom') {
     joinGameRoom(args[1], args[2], args[3]);
 }
 
-// node index.js leaveGam 6 sessionJwt
+// node index.js leaveGameRoom roomId sessionJwt
 // args1 = roomId
 // args2 = plyrId
 // args3 = sessionJwt
@@ -118,3 +116,51 @@ else if (args[0] == 'leaveGameRoom') {
     leaveGameRoom(args[1], args[2], args[3]);
 }
 
+// node index.js approveGameToken plyrId gameId plyr 10 3600 123456
+// args1 = plyrId
+// args2 = gameId / Not a game api key / you can get it with api key, sec key
+// args3 = token name plyr or gamr and support token address in the future
+// args4 = amount of token to approve
+// args5 = expiresIn
+// args6 = otp // 2fa token
+// Once you approved, it means that it approved to entire game process. Don't need to call it everytime to pay to each room
+else if (args[0] == 'approveGameToken') {
+    approveGameToken(args[1], args[2], args[3], args[4], args[5], args[6]);
+}
+
+// node index.js payGameRoom roomId plyrId sessionJwt tokenName amount
+// args1 = roomId
+// args2 = plyrId
+// args3 = sessionJwt
+// args4 = tokenName (now support only 'plyr', 'gamr' and future it will support more and ttoken address too)
+// args5 = amount of token to approve (Just a number like 0.00001 , 1 , 10 , 1000)
+// In the real world, you can spend many users' tokens at the same time (please check at lib/game.js)
+else if (args[0] == 'payGameRoom') {
+    payGameRoom(args[1], args[2], args[3], args[4], args[5])
+}
+
+// node index.js payGameRoom roomId plyrId sessionJwt tokenName amount
+// args1 = roomId
+// args2 = plyrId
+// args3 = tokenName (now support only 'plyr', 'gamr' and future it will support more and ttoken address too)
+// args4 = amount of token to approve (Just a number like 0.00001 , 1 , 10 , 1000)
+// If you want to broadcast earning. Please should use Multicall function or loop by yourself
+else if (args[0] == 'earnGameRoom') {
+    earnGameRoom(args[1], args[2], args[3], args[4])
+}
+
+else if (args[0] == 'endGameRoom') {
+
+}
+
+else if (args[0] == 'cancelGameRoom') {
+
+}
+
+else if (args[0] == 'allowanceGameToken') {
+
+}
+
+else if (args[0] == 'isJoinedGameRoom') {
+
+}
